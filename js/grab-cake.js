bcd7c200bba999c7b23588594a1724d69590c43e
StoneC <rockinfog@hotmail.com>
2018-04-06 01:23:14 +0800

    自动买入

--- js/grab-cake.js
+++ js/grab-cake.js
@@ -7,6 +7,16 @@ function grabCake() {
 
     console.log('----grapCake now');
 
+    chrome.storage.local.get('distance', function (store) {
+        console.log('config distance:' + store.distance);
+        config.distance = store.distance;
+    });
+
+    chrome.storage.local.get('cnt', function (store) {
+        console.log('config cnt:' + store.cnt);
+        config.cnt = store.cnt;
+    });
+
     // 轮询
     var timer = setInterval(function () {
 
@@ -14,6 +24,13 @@ function grabCake() {
             console.log('config distance:' + store.distance);
             config.distance = store.distance;
         });
+
+        chrome.storage.local.get('cnt', function (store) {
+            console.log('config cnt:' + store.cnt);
+            config.cnt = store.cnt;
+        });
+
+
 
         var arrIndexBuy = [15, 16, 17, 18, 19];
         var arrBuy = [];
@@ -41,34 +58,60 @@ function grabCake() {
             //console.log(sellItem);
             arrSell.push(sellItem);
         });
-
-        var chajia = parseInt(arrSell[0].cny) - parseInt(arrBuy[arrBuy.length - 1].cny);
+        var btccnt = parseFloat(arrBuy[arrBuy.length - 1].btc);
+        console.log('btccnt:' + btccnt)
+        var sellAmount = parseFloat(arrSell[1].amount) + parseFloat(arrSell[2].amount)
+            + parseFloat(arrSell[3].amount) + parseFloat(arrSell[4].amount);
+        var sellBtc = parseFloat(arrSell[1].btc) + parseFloat(arrSell[2].btc)
+            + parseFloat(arrSell[3].btc) + parseFloat(arrSell[4].btc);
+        var sellPrice = sellAmount / sellBtc;
+        //console.log('sellPrice:' + sellPrice)
+        var chajia = sellPrice - parseInt(arrBuy[arrBuy.length - 1].cny);
         console.log('目前的差价是:' + chajia);
         console.log('配置的差价是:' + config.distance);
-        if (parseInt(chajia) > parseInt(config.distance)) {
-            console.log('差价出现了');
+        // console.log(arrBuy[0]);
+        // console.log(arrBuy[arrBuy.length - 1]);
+        if (btccnt >= 0.1 && parseInt(chajia) > parseInt(config.distance)) {
+            var date = new Date()
+            console.log('差价出现了:' + chajia + ",时间：" + date.toString());
+            config.cnt++;
+            console.log('累计出现次数：' + config.cnt);
+            console.log(arrSell[1]);
+            console.log(arrSell[2]);
+            console.log(arrSell[3]);
+            console.log(arrSell[4]);
+
+            console.log(arrBuy[arrBuy.length - 1]);
+            chrome.storage.local.set({ 'cnt': config.cnt }, function () {
+                //console.log('存储累计成功');
+            });
             chrome.runtime.sendMessage({ "command": "play" },
                 function (response) {
-                    console.log("bg log responsed:" + response.result);
+                    //console.log("bg log responsed:" + response.result);
                 });
+
+            var $btn = document.querySelectorAll('li.buy>span>button')[19];
+            //console.log($btn);
+            if ($btn) {
+                fireEvent($btn, 'click');
+             
+                //console.log();
+                alert('已自动下单，请尽快操作！');
+
+                var $sureBtn = document.querySelectorAll('.sure-order>div.btn-group>button')[1];
+                fireEvent($sureBtn,'click');
+                clearInterval(timer);
+                return;
+            }
         }
         else {
-            console.log('未达到差价要求');
+            //console.log('未达到差价要求');
             chrome.runtime.sendMessage({ "command": "pause" },
                 function (response) {
-                    console.log("bg log responsed:" + response.result);
+                    //console.log("bg log responsed:" + response.result);
                 });
         }
-        var $btn = document.querySelectorAll('li.buy>span>button')[0];
-        //console.log($btn);
 
-
-
-        if ($btn) {
-            //fireEvent($btn, 'click');
-            //clearInterval(timer);
-            return;
-        }
     }, 2000)
 }
 
--- popup.js
+++ popup.js
@@ -4,10 +4,21 @@ var bg = chrome.extension.getBackgroundPage();
 //bg.test();
 
 
+chrome.storage.local.get('distance', function (store) {
+    if(store&& store.distance);
+    {
+        document.getElementById('distance').value=store.distance;
+    }
+});
+
+
 document.getElementById("btnSubmit").onclick = function () {
     var distance = document.getElementById('distance').value;
-    chrome.storage.local.set({'distance': distance}, function() {
-        console.log('存储成功') ;      
-});
+    chrome.storage.local.set({ 'distance': distance }, function () {
+        console.log('存储成功');
+    });
+    chrome.storage.local.set({ 'cnt': 0 }, function () {
+        console.log('存储成功');
+    });
 }
 
